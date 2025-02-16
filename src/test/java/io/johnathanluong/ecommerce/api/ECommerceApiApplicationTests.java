dad8589fda95d87363ff4e04481d10c09cac0ac8
John Luong <johnathanluong7@gmail.com>
2025-02-16 18:31:13 -0500

    Spring boot initial

---
+++ src/test/java/io/johnathanluong/ecommerce/api/ECommerceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.johnathanluong.ecommerce.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
